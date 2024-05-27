36abca95e1f1da9218ce16124c022115217582e5
HsienLu <hsienlu@mail.ntust.edu.tw>
2024-05-27 11:41:39 +0800

    找到插入JSP後無法讀取靜態資源的解決方法，所以移除header

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,11 @@ import StartingGuideExecuteAlice from './component/StartingGuideExecuteAlice'
 import StartingGuideSetStudentTestAccount from './component/StartingGuideSetStudentTestAccount'
 import TeacherToolsClassShow from './component/TeacherToolsClassShow'
 import TeacherToolsAssessment from './component/TeacherToolsAssessment'
-import Header from './component/Header'
 import ToTheTop from './component/ToTheTop'
 import { Routes,Route } from 'react-router-dom'
 function App() {
   return (
     <>
-      
-      <Header></Header>
       <div className="container mx-auto flex justify-center mt-8">
         <DropDownManual className='mr-16'></DropDownManual>
         <Routes>
