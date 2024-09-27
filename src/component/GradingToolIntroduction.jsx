8dbe385fa11927e200a1e9fab82a66be4341c688
HsienLu <136676022+HsienLu@users.noreply.github.com>
2024-09-27 15:42:21 +0800

    Add GradingToolIntroduction images and update GradingToolIntroduction component

--- src/component/GradingToolIntroduction.jsx
+++ src/component/GradingToolIntroduction.jsx
@@ -1,7 +1,32 @@
+import GradingToolIntroduction1 from '../assets/GradingToolIntroduction/GradingToolIntroduction26.png'
+import GradingToolIntroduction2 from '../assets/GradingToolIntroduction/GradingToolIntroduction27.png'
+import GradingToolIntroduction3 from '../assets/GradingToolIntroduction/GradingToolIntroduction28.png'
+import GradingToolIntroduction4 from '../assets/GradingToolIntroduction/GradingToolIntroduction29.png'
+import GradingToolIntroduction5 from '../assets/GradingToolIntroduction/GradingToolIntroduction30.png'
+import GradingToolIntroduction6 from '../assets/GradingToolIntroduction/GradingToolIntroduction31.png'
+import GradingToolIntroduction7 from '../assets/GradingToolIntroduction/GradingToolIntroduction32.png'
+import GradingToolIntroduction8 from '../assets/GradingToolIntroduction/GradingToolIntroduction33.png'
+import GradingToolIntroduction9 from '../assets/GradingToolIntroduction/GradingToolIntroduction34.png'
+import GradingToolIntroduction10 from '../assets/GradingToolIntroduction/GradingToolIntroduction35.png'
+import GradingToolIntroduction11 from '../assets/GradingToolIntroduction/GradingToolIntroduction36.png'
+import GradingToolIntroduction12 from '../assets/GradingToolIntroduction/GradingToolIntroduction37.png'
+import GradingToolIntroduction13 from '../assets/GradingToolIntroduction/GradingToolIntroduction38.png'
+
+import ContentCard from './ContentCard';
+
+
 function GradingToolIntroduction(){
+    const images = [GradingToolIntroduction1,GradingToolIntroduction2,GradingToolIntroduction3,GradingToolIntroduction4,GradingToolIntroduction5,GradingToolIntroduction6,GradingToolIntroduction7,GradingToolIntroduction8,GradingToolIntroduction9,GradingToolIntroduction10,GradingToolIntroduction11,GradingToolIntroduction12,GradingToolIntroduction13];
     return(
         <>
-            <h1>GradingToolIntroduction</h1>
+        <div className="basis-2/3">
+        {
+            images.map((v,i)=>{
+
+             return(   <ContentCard key={i} imgSrc={v}></ContentCard>)
+            })
+        }
+        </div>
         </>
     )
 }
