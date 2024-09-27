ce606c45b13189587ad5c46f2c5070b85e3a1f2d
HsienLu <136676022+HsienLu@users.noreply.github.com>
2024-09-27 15:01:23 +0800

    Add image imports and map images in CourseImplementation and CoursePresentation components

--- src/component/CourseImplementation.jsx
+++ src/component/CourseImplementation.jsx
@@ -1,7 +1,38 @@
+import CourseImplementationImg1 from '../assets/CourseImplementation/CourseImplementation-4.png';
+import CourseImplementationImg2 from '../assets/CourseImplementation/CourseImplementation-5.png';
+import CourseImplementationImg3 from '../assets/CourseImplementation/CourseImplementation-6.png';
+import CourseImplementationImg4 from '../assets/CourseImplementation/CourseImplementation-7.png';
+import CourseImplementationImg5 from '../assets/CourseImplementation/CourseImplementation-8.png';
+import CourseImplementationImg6 from '../assets/CourseImplementation/CourseImplementation-9.png';
+import CourseImplementationImg7 from '../assets/CourseImplementation/CourseImplementation-10.png';
+import CourseImplementationImg8 from '../assets/CourseImplementation/CourseImplementation-11.png';
+import CourseImplementationImg9 from '../assets/CourseImplementation/CourseImplementation-12.png';
+import CourseImplementationImg10 from '../assets/CourseImplementation/CourseImplementation-13.png';
+import CourseImplementationImg11 from '../assets/CourseImplementation/CourseImplementation-14.png';
+import CourseImplementationImg12 from '../assets/CourseImplementation/CourseImplementation-15.png';
+import CourseImplementationImg13 from '../assets/CourseImplementation/CourseImplementation-16.png';
+import CourseImplementationImg14 from '../assets/CourseImplementation/CourseImplementation-17.png';
+import CourseImplementationImg15 from '../assets/CourseImplementation/CourseImplementation-18.png';
+import CourseImplementationImg16 from '../assets/CourseImplementation/CourseImplementation-19.png';
+import CourseImplementationImg17 from '../assets/CourseImplementation/CourseImplementation-20.png';
+import CourseImplementationImg18 from '../assets/CourseImplementation/CourseImplementation-21.png';
+
+
+
+import ContentCard from './ContentCard';
+
 function CourseImplementation(){
+    const images =[CourseImplementationImg1,CourseImplementationImg2,CourseImplementationImg3,CourseImplementationImg4,CourseImplementationImg5,CourseImplementationImg6,CourseImplementationImg7,CourseImplementationImg8,CourseImplementationImg9,CourseImplementationImg10,CourseImplementationImg11,CourseImplementationImg12,CourseImplementationImg13,CourseImplementationImg14,CourseImplementationImg15,CourseImplementationImg16,CourseImplementationImg17,CourseImplementationImg18];
     return(
         <>
-            <h1>CourseImplementation</h1>
+        <div className="basis-2/3">
+        {
+            images.map((v,i)=>{
+
+             return(   <ContentCard key={i} imgSrc={v}></ContentCard>)
+            })
+        }
+        </div>
         </>
     )
 }
--- src/component/CoursePresentation.jsx
+++ src/component/CoursePresentation.jsx
@@ -1,7 +1,21 @@
+import CoursePresentation1 from '../assets/CoursePresentation/CoursePresentation22.png';
+import CoursePresentation2 from '../assets/CoursePresentation/CoursePresentation23.png';
+import CoursePresentation3 from '../assets/CoursePresentation/CoursePresentation24.png';
+
+import ContentCard from './ContentCard';
+
 function CoursePresentation(){
+    const images =[CoursePresentation1,CoursePresentation2,CoursePresentation3];
     return(
         <>
-            <h1>CoursePresentation</h1>
+        <div className="basis-2/3">
+        {
+            images.map((v,i)=>{
+
+             return(   <ContentCard key={i} imgSrc={v}></ContentCard>)
+            })
+        }
+        </div>
         </>
     )
 }
